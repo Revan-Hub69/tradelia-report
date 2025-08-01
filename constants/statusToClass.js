8e2737718ba87eb645ebfd5513ac548a148cfed4
Revan-Hub69 <topofferte@protonmail.ch>
2025-08-01 18:15:12 +0200

    Create statusToClass.js

---
+++ constants/statusToClass.js
@@ -0,0 +1,6 @@
+export const statusToClass = {
+  positivo: "text-green-600 font-bold",
+  warning: "text-yellow-600 font-bold",
+  negativo: "text-red-600 font-bold",
+  neutro: "text-slate-700 font-bold"
+};
