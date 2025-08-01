403d8c023691079de3b65fd22823982dedd86fe2
Revan-Hub69 <topofferte@protonmail.ch>
2025-08-01 18:18:13 +0200

    Create tooltipsBloccoX.js

---
+++ constants/tooltipsBloccoX.js
@@ -0,0 +1,32 @@
+// /constants/tooltipsAll.js
+import { tooltipsBlocco1 } from "./tooltipsBlocco1";
+import { tooltipsBlocco2 } from "./tooltipsBlocco2";
+import { tooltipsBlocco3 } from "./tooltipsBlocco3";
+import { tooltipsBlocco4 } from "./tooltipsBlocco4";
+import { tooltipsBlocco5 } from "./tooltipsBlocco5";
+import { tooltipsBlocco6 } from "./tooltipsBlocco6";
+import { tooltipsBlocco7 } from "./tooltipsBlocco7";
+import { tooltipsBlocco8 } from "./tooltipsBlocco8";
+import { tooltipsBlocco9 } from "./tooltipsBlocco9";
+import { tooltipsBlocco10 } from "./tooltipsBlocco10";
+import { tooltipsBlocco11 } from "./tooltipsBlocco11";
+import { tooltipsBlocco12 } from "./tooltipsBlocco12";
+import { tooltipsBlocco13 } from "./tooltipsBlocco13";
+import { tooltipsBlocco14 } from "./tooltipsBlocco14";
+
+export const tooltips = {
+  blocco1: tooltipsBlocco1,
+  blocco2: tooltipsBlocco2,
+  blocco3: tooltipsBlocco3,
+  blocco4: tooltipsBlocco4,
+  blocco5: tooltipsBlocco5,
+  blocco6: tooltipsBlocco6,
+  blocco7: tooltipsBlocco7,
+  blocco8: tooltipsBlocco8,
+  blocco9: tooltipsBlocco9,
+  blocco10: tooltipsBlocco10,
+  blocco11: tooltipsBlocco11,
+  blocco12: tooltipsBlocco12,
+  blocco13: tooltipsBlocco13,
+  blocco14: tooltipsBlocco14
+};
